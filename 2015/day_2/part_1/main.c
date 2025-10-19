88d4787dc1f555d3cd9c492689a61d06fbb23e35
Maya Weber <mayamaya124816@tutamail.com>
2025-10-19 18:23:41 +0200

    2015: Add day_2 solutions

---
+++ 2015/day_2/part_1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int get_smallest_area(int l, int w, int h) {
+    int a1 = l*w;
+    int a2 = w*h;
+    int a3 = h*l;
+
+    int smallest = a1;
+    if (a2 < smallest) smallest = a2;
+    if (a3 < smallest) smallest = a3;
+    return smallest;
+}
+
+int calculate_surface_area(int l, int w, int h) {
+    return 2*l*w + 2*w*h + 2*h*l;
+}
+
+int main() {
+    FILE *fptr = fopen("input", "r");
+
+    int l, w, h, result = 0;
+    while (fscanf(fptr, "%dx%dx%d", &l, &w, &h) != EOF) {
+        result += calculate_surface_area(l, w, h) + get_smallest_area(l, w, h);
+    }
+    printf("Result: %d\n", result);
+ 
+    fclose(fptr);
+    return 0;
+}
---
+++ 2015/day_2/part_2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int get_smallest_ribbon(int l, int w, int h) {
+    int o1 = 2 * l + 2 * w;
+    int o2 = 2 * l + 2 * h;
+    int o3 = 2 * w + 2 * h;
+
+    int smallest = o1;
+    if (o2 < smallest) smallest = o2;
+    if (o3 < smallest) smallest = o3;
+    return smallest;
+}
+
+int get_volume(int l, int w, int h) {
+    return l * w * h;
+}
+
+int main() {
+    FILE *fptr = fopen("input", "r");
+
+    int l, w, h, result = 0;
+    while (fscanf(fptr, "%dx%dx%d", &l, &w, &h) != EOF) {
+        result += get_smallest_ribbon(l, w, h) + get_volume(l, w, h);
+    }
+    printf("Result: %d\n", result);
+ 
+    fclose(fptr);
+    return 0;
+}
